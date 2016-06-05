9df96c6fb35cd35b22e6fe91c4d28e560a6f32e8
Rakavy Ravisingam <rakavy_12@hotmail.com>
2016-06-04 20:17:02 -0400

    Changed structureee and added style/ js

---
+++ web/server.js
@@ -0,0 +1,31 @@
+// load the things we need
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page 
+app.get('/', function(req, res) {
+  var fs = require("fs");
+  var contents = fs.readFileSync("events.json");
+  var jsonContent = JSON.parse(contents);
+	
+    var tagline = jsonContent[0]["eventName"];
+
+    res.render('pages/index', {
+        drinks: jsonContent,
+        tagline: tagline
+    });
+});
+// about page 
+app.get('/about', function(req, res) {
+	res.render('pages/about');
+});
+
+app.use(express.static(__dirname + '/stylesheet'));
+
+app.listen(8080);
+console.log('8080 is the magic port');
