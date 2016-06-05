1758cfc0881e7861af5585434257e4e5c9754b02
avinanda <avinanda.chatt@live.com>
2016-06-05 07:32:44 -0400

    Added confirmation messages for admin and css fixes

--- web/server.js
+++ web/server.js
@@ -21,12 +21,12 @@ app.get('/event/:id', function (req, res) {
   var jsonContent = JSON.parse(contents)
   var id = req.param('id');
   idEvent = id;
-  var tagline = jsonContent[id]["name"];
-  var admin = jsonContent[id]["owner"]
-  var location = jsonContent[id]["location"];
-  var desc = jsonContent[id]["description"];
-    var type = jsonContent[id]["type"];
-  var time = jsonContent[id]["time"];
+  var tagline = (jsonContent[id]["name"] || '');
+  var admin = (jsonContent[id]["owner"] || '');
+  var location = (jsonContent[id]["location"] || '');
+  var desc = (jsonContent[id]["description"] || '');
+  var type = (jsonContent[id]["type"] || '');
+  var time = (jsonContent[id]["time"] || '');
   req.params.id = jsonContent[id]["id"]
   res.render('pages/admin', {
     drinks: jsonContent,
@@ -46,13 +46,13 @@ app.get('/event/:id/invite/:invitee', function (req, res) {
   idEvent = id;
   var invitee = req.param('invitee');
   invite = invitee;
-  var name = jsonContent[id]["name"];
-  var location = jsonContent[id]["location"];
-  var type = jsonContent[id]["type"];
-  var time = jsonContent[id]["time"];
-  var description = jsonContent[id]["description"];
-  var room = jsonContent[id]["room"];
-  var admin = jsonContent[id]["owner"];
+  var name = (jsonContent[id]["name"] || '');
+  var location = (jsonContent[id]["location"] || '');
+  var type = (jsonContent[id]["type"] || '');
+  var time = (jsonContent[id]["time"] || '');
+  var description = (jsonContent[id]["description"] || '');
+  var room = (jsonContent[id]["room"] || '');
+  var admin = (jsonContent[id]["owner"] || '');
   res.render('pages/index', {
     name: name,
     location: location,
@@ -100,12 +100,20 @@ app.post('/save', function (req, res) {
 
   var desc = req.body.desc;
   var name = req.body.eventName;
+  var loc = req.body.loc;
+  var type = req.body.type;
+  var admin = req.body.admin;
+  var time = req.body.time;
   var events;
   fs.readFile("../data/events.json", 'utf8', function (err, data) {
     events = JSON.parse(data);
     events[idEvent].description = desc;
     events[idEvent].name = name;
-
+    events[idEvent].location = loc;
+    events[idEvent].type = type;
+    events[idEvent].time = time;
+    events[idEvent].admin = admin;
+     
     fs.writeFile("../data/events.json", JSON.stringify(events), function (error) {
       if (error) {
         console.log("Error:" + error);
